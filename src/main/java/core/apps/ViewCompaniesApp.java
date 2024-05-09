61985c3ef8bcb52ca5e0ef01e33304b9627fc7e7
David <dperezfernandez@safareyes.es>
2024-05-09 17:19:07 +0200

    ♻️ Refactor files

--- src/main/java/core/apps/ViewCompaniesApp.java
+++ src/main/java/core/apps/ViewCompaniesApp.java
@@ -1,20 +1,31 @@
 package core.apps;
 
+import core.controllers.ViewCompaniesController;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 import java.io.IOException;
+import java.sql.SQLException;
+
 
 public class ViewCompaniesApp extends Application {
+    private Scene scene;
     @Override
-    public void start(Stage stage) throws IOException {
+    public void start(Stage stage) throws IOException, SQLException {
         FXMLLoader fxmlLoader = new FXMLLoader(ViewCompaniesApp.class.getResource("/pages/viewCompanies.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
+        scene = new Scene(fxmlLoader.load(), 800, 500);
+        stage.setTitle("Empresas - Prácticas Empresariales SAFA");
         stage.setScene(scene);
         stage.show();
+
+        // Add the stylesheet to the scene
+        scene.getStylesheets().add(String.valueOf(ViewCompaniesApp.class.getResource("/css/bootstrap.css")));
+
+        // Initialize the table with the companies
+        ViewCompaniesController controller = fxmlLoader.getController();
+        controller.start();
     }
 
     public static void main(String[] args) {
--- src/main/java/core/controllers/ViewCompaniesController.java
+++ src/main/java/core/controllers/ViewCompaniesController.java
@@ -1,4 +1,38 @@
 package core.controllers;
 
+import api.models.Company;
+import core.utils.TableViewManager;
+import javafx.fxml.FXML;
+import javafx.scene.control.TableView;
+
+import java.sql.SQLException;
+import java.util.List;
+
+import static api.controllers.CompanyController.getCompanies;
+
 public class ViewCompaniesController {
+    @FXML
+    private TableView companiesTable;
+
+    public void start() throws SQLException {
+        // Initialize the table with the companies
+        addCompaniesToTable(companiesTable);
+    }
+
+    public void addCompaniesToTable(TableView companiesTable) throws SQLException {
+        List<Company> companies = getCompanies();
+        TableViewManager<Company> tableViewManager = new TableViewManager<>(companiesTable);
+
+        tableViewManager.addColumn("Código", Company::getCompanyCode);
+        tableViewManager.addColumn("CIF", Company::getCif);
+        tableViewManager.addColumn("Nombre", Company::getName);
+        tableViewManager.addColumn("Dirección", Company::getDirection);
+        tableViewManager.addColumn("Código Postal", Company::getPostalCode);
+        tableViewManager.addColumn("Localidad", Company::getLocation);
+        tableViewManager.addColumn("Jornada", company -> company.getWorkday().getName());
+        tableViewManager.addColumn("Modalidad", company -> company.getModality().getName());
+        tableViewManager.addColumn("Email", Company::getEmail);
+
+        tableViewManager.addAllData(companies);
+    }
 }
