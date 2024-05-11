3c0b060e9a5059ecbb5a9248b6ba48dfcffe6d82
david <dperezfernandez@safareyes.es>
2024-05-11 11:52:42 +0200

    ✨ Added the event to open view companies

--- src/main/java/core/controllers/IndexController.java
+++ src/main/java/core/controllers/IndexController.java
@@ -1,8 +1,14 @@
 package core.controllers;
 
+import core.apps.ViewCompaniesApp;
 import javafx.fxml.FXML;
 import javafx.scene.image.ImageView;
+import javafx.stage.Stage;
 
+import java.io.IOException;
+import java.sql.SQLException;
+
+import static api.services.CompanyService.getAllCompanies;
 import static core.utils.ImageLoader.loadImage;
 
 public class IndexController {
@@ -20,5 +26,17 @@ public class IndexController {
         loadImage(personImage, "/media/Study-abroad-pana.png");
         loadImage(companyImage, "/media/City-skyline-pana.png");
         loadImage(asignationImage, "/media/Back-to-back-pana.png");
+    }
+
+    @FXML
+    public void seeAllCompanies() throws SQLException {
+        ViewCompaniesApp viewCompaniesApp = new ViewCompaniesApp(getAllCompanies());
+        try {
+            viewCompaniesApp.start(new Stage());
+            Stage stage = (Stage) personImage.getScene().getWindow();
+            stage.close();
+        } catch (IOException | SQLException ioException) {
+            ioException.printStackTrace();
+        }
     }
 }
